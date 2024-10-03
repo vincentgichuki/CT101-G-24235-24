81d80b0440f2f7c06be924aad73db438e91da675
vincentgichuki <gichuki.24235@students.kyu.ac.ke>
2024-10-03 08:57:42 +0300

    Update quiz1.c

--- quiz1.c
+++ quiz1.c
@@ -3,12 +3,14 @@
 int main()
 {
     int age, income;
-
+    
+    //Age and annual income user input
     printf("Please Enter your age:");
     scanf("%d", &age);
     printf("Please Enter your annual income:");
     scanf("%d", &income);
-
+    
+    //conditional statement
     if (age>=21)
     {
         if (income>=21000)
